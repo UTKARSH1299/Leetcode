b3058989c34c37ae46522c42fecd3441b8f8829b
UTKARSH1299 <147276803+UTKARSH1299@users.noreply.github.com>
2025-10-22 17:13:04 +0530

    Time: 12 ms (7.68%) | Memory: 27.4 MB (7.61%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+bool checkPalindrome(string str,int s, int e){
+   while(s<=e){
+    if(str[s]!=str[e]){
+        return false;
+    }
+    else{
+        s++;
+        e--;
+    }
+   }
+   return true;
+}
+    bool validPalindrome(string s) {
+        int len=s.length();
+        int i=0;
+        int j=len-1;
+
+        while(i<=j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                //s[i]!=s[j]
+
+                bool ans1=checkPalindrome(s,i+1,j);
+                bool ans2=checkPalindrome(s,i,j-1);
+               bool check=ans1 || ans2;
+               return check;
+            
+            }
+        }
+        return true;
+    }
+};

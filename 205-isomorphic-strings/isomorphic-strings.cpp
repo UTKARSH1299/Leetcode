2d046981e3eaaefd471ca757e526c355fcc8ff39
UTKARSH1299 <147276803+UTKARSH1299@users.noreply.github.com>
2025-11-27 12:04:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (92.63%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+        bool ischarMapped[256]={0};
+
+        for(int i=0; i<s.size(); i++){
+            if(hash[s[i]]==0 && ischarMapped[t[i]]==0){
+                 hash[s[i]]=t[i];
+                 ischarMapped[t[i]]=true;
+            }
+        }
+        for(int i=0; i<s.size(); i++){
+            if(char(hash[s[i]])!=t[i]){
+                return false;
+
+            }
+        }
+        return true;
+    }
+};

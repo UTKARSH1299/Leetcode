210c1efd34b39ee265ef28642fff9f8055d57873
UTKARSH1299 <147276803+UTKARSH1299@users.noreply.github.com>
2026-05-26 00:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int freq[256]={0};
-        for(int i=0; i<s.size(); i++){
-            freq[s[i]]++;
+
+        int hash[256]={0};
+
+        for(int i=0; i<s.size();i++){
+            hash[s[i]]++;
         }
+
+
         for(int i=0; i<t.size(); i++){
-            freq[t[i]]--;
+            hash[t[i]]--;
         }
+
         for(int i=0; i<256; i++){
-            if(freq[i]!=0){
+
+            if(hash[i]!=0){
                 return false;
             }
-            
         }
         return true;
+        
     }
 };
